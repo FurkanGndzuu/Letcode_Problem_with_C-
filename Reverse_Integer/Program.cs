3ae144e603483b74dfa49b9c970f0c29d3dd28df
Furkangndz93 <furkangndz93@gmail.com>
2023-03-12 21:56:39 +0300

    commit 3

---
+++ Reverse_Integer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Integer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //This Codes for Test
+
+            Random random = new Random();
+
+            int a = random.Next(1, 10000);
+
+            if(random.Next(0,2) == 1)
+            {
+                a *= (-1);
+            }
+
+            Console.WriteLine(a);
+            Console.WriteLine(Solution.Reverse(a));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Reverse_Integer/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Integer
+{
+    public static class Solution
+    {
+        public static int Reverse(int x)
+        {
+            string b="" , a = x.ToString();
+
+            bool Is_There_Any_Minus = false;
+
+            for(int i = a.Length - 1; i >=0 ; i--)
+            {
+                if (a[i] == '-')
+                {
+                    Is_There_Any_Minus=true;
+                    continue;
+                }
+                b += a[i];
+            }
+
+            if(Is_There_Any_Minus)
+                return (Convert.ToInt32(b) * (-1));
+            else return Convert.ToInt32(b);
+        }
+    }
+}
