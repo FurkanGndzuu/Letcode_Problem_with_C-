408aefb8d4a85a53fdc24322194d3ad70172d8a3
Furkangndz93 <furkangndz93@gmail.com>
2023-03-12 21:31:02 +0300

    commit 2

---
+++ Median_of_Two_Sorted_Arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Median_of_Two_Sorted_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //This codes for test
+
+            Random random = new Random();
+            int[] array1 = new int[random.Next(1, 5)];
+            int[] array2 = new int[random.Next(1, 5)];
+
+            for(int i =0; i < array1.Length; i++)
+            {
+                array1[i] = random.Next(1,100);
+                Console.Write(array1[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                array2[i] = random.Next(1, 100);
+                Console.Write(array2[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(Solution.FindMedianSortedArrays(array1, array2));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Median_of_Two_Sorted_Arrays/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Median_of_Two_Sorted_Arrays
+{
+    public static class Solution
+    {
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int[] nums = new int[nums1.Length + nums2.Length];
+
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                nums[i] = nums1[i];
+            }
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                nums[nums1.Length + i] = nums2[i];
+            }
+
+            Array.Sort(nums);
+
+            int a = nums.Length / 2;
+
+            if (nums.Length % 2 == 0)
+                return (double)(nums[a] + nums[a - 1]) / 2;
+            else return nums[a];
+                
+            }
+        }
+}
