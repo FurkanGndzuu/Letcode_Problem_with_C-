7bc352aa62f99b49287a5df018de46996b965fc2
Furkangndz93 <furkangndz93@gmail.com>
2023-03-15 10:11:47 +0300

    commit 5

---
+++ RemoveNthFromEnd/Program.cs
@@ -0,0 +1,52 @@
+﻿using RemoveNthFromEnd;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //This codes for test
+
+        Random random = new Random();
+
+        ListNode head = new(random.Next(1,10));
+        Console.Write(head.val + " => ");
+
+        for(int i = 1; i <= 10; i++)
+        {
+            ListNode newNode = new(random.Next(1,50));
+            AddLinkedList(head, newNode);
+            Console.Write(newNode.val+ " => ");
+        }
+
+      head =  Solution.result(head, random.Next(1, 10));
+
+        Console.WriteLine("\nAfter Removed");
+
+        while(head != null)
+        {
+            Console.Write(head.val + " => ");
+            head = head.next;
+        }
+
+        Console.ReadLine();
+
+    }
+
+    public static void AddLinkedList(ListNode head , ListNode newNode)
+    {
+        ListNode curr = head;
+
+        if(curr.next == null)
+        {
+            curr.next = newNode;
+        }
+        else
+        {
+            while(curr.next != null)
+            {
+                curr = curr.next;
+            }
+            curr.next = newNode;
+        }
+    }
+}
---
+++ RemoveNthFromEnd/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveNthFromEnd
+{
+    public static class Solution
+    {
+        public static ListNode result(ListNode head , int n)
+        {
+            ListNode current = head;
+
+            if(n == 1 && current.next != null)
+            {
+                head = current.next;
+                current = null;
+            }
+            else
+            {
+                int i = 1;
+                ListNode prev = null;
+                while (i <= n && current != null)
+                {
+                     prev = current;
+                    current = current.next;
+                    i++;
+                }
+                prev.next = current.next;
+            }
+
+            return head;
+        }
+    }
+}
