3ffaae602322e753f86de208b0a90829f3e8a964
Furkangndz93 <furkangndz93@gmail.com>
2023-06-10 16:27:30 +0300

    commit

---
+++ MinStack/MinStack/MinStack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinStack
+{
+    public class MinStack
+    {
+        Stack<int> stack;
+        public MinStack()
+        {
+            stack = new Stack<int>();
+        }
+
+        public void Push(int val)
+        {
+            stack.Push(val);
+        }
+
+        public void Pop()
+        {
+            stack.Pop();
+        }
+
+        public int Top()
+        {
+            return stack.Peek();
+        }
+
+        public int GetMin() => stack.OrderBy(x => x).FirstOrDefault();
+    }
+}
