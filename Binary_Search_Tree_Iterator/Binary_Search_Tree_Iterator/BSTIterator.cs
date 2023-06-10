74d550d1e05357d16ca4cec26b54160c026ab038
Furkangndz93 <furkangndz93@gmail.com>
2023-06-10 16:15:37 +0300

    commit

---
+++ Binary_Search_Tree_Iterator/Binary_Search_Tree_Iterator/BSTIterator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_Tree_Iterator
+{
+    class TreeNode
+    {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+     class BSTIterator
+    {
+
+        public TreeNode rootNode { get; set; }
+        public TreeNode currentNode  { get; set; }
+        public IList<TreeNode> TreeNodes { get; set; }
+        public int Counter { get; set; }
+        public BSTIterator(TreeNode root)
+        {
+            rootNode = root;
+            currentNode = root;
+            Counter = 0;
+            TreeNodes = inOrder(root, new List<TreeNode>());
+        }
+
+        public int Next()
+        {
+           if(Counter <= TreeNodes.Count)
+            {
+                int response = TreeNodes.ElementAtOrDefault(Counter).val;
+                Counter++;
+                return response;
+            }
+            throw new Exception();
+        }
+
+        public bool HasNext()
+        {
+            if(Counter + 1 <= TreeNodes.Count)
+                return true;
+            return false;
+        }
+
+        private IList<TreeNode> inOrder (TreeNode root , IList<TreeNode> InOrderNodes)
+        {
+            if (root.left is not null)
+                inOrder(root.left, InOrderNodes);
+
+            if (root is not null)
+                InOrderNodes.Add(root);
+          
+            if (root.right is not null)
+                inOrder(root.right, InOrderNodes);
+
+            return InOrderNodes;
+        }
+    }
+}
---
+++ Binary_Search_Tree_Iterator/Binary_Search_Tree_Iterator/Program.cs
@@ -0,0 +1,26 @@
+﻿using Binary_Search_Tree_Iterator;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node1 = new TreeNode(9);
+        TreeNode node2 = new TreeNode(20);
+        TreeNode treeNode = new TreeNode(15 , node1 , node2);
+        TreeNode root = new TreeNode(7 , node3 , treeNode);
+
+        BSTIterator bst = new BSTIterator(root);
+        Console.WriteLine(bst.Next());
+        Console.WriteLine(bst.Next());
+        Console.WriteLine(bst.HasNext());
+        Console.WriteLine(bst.Next());
+        Console.WriteLine(bst.HasNext());
+        Console.WriteLine(bst.Next());
+        Console.WriteLine(bst.HasNext());
+        Console.WriteLine(bst.Next());
+        Console.WriteLine(bst.HasNext());
+       
+    }
+}
